aa1dea918ccf902ab71f4e3928a1ae97b433dc89
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-05-02 12:30:15 +0530

    constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+# All application constants should be defined here
+
+SHEET_NAME = "Placed_Dashboard"
+WORKSHEET_INDEX = 0
+REFRESH_SECONDS = 30
+
+# set USE_CSV=True for local testing
+USE_CSV = False
+CSV_FILE = "Placed_Dashboard - Form responses 1.csv"
