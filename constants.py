a156f3805f48f35910a293487d31f7e528efc7ea
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-05-04 20:25:32 +0530

    constants

--- constants.py
+++ constants.py
@@ -1,10 +1,11 @@
 # All application constants should be defined here
 
-SHEET_NAME = "Placed_Dashboard"
+SHEET_NAME = "STUDENTS_DATA"
 WORKSHEET_INDEX = 0
 REFRESH_SECONDS = 30
 
 # Use new CSV dataset
 USE_CSV = True
+MAIN_CSV = "STUDENTS - Sheet1"
 CSV_FILE = "CLASSIFICATIONS - Sheet1 (1).csv"
 
