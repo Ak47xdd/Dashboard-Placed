9130853471742c477f3118ef1f7ba3e03e206189
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-05-06 17:25:18 +0530

    app

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import streamlit as st
-import gspread
-from google.oauth2 import service_account
-import pandas as pd
-import plotly.express as px
 from datetime import datetime
-import time
 from frontend.bg import set_bg_image
-from client import get_clients
 from data.data import *
 from frontend.styles import load_styles, set_page_config, auto_refresh
 from filter import filter_data
-from constants import SHEET_NAME, WORKSHEET_INDEX, REFRESH_SECONDS, USE_CSV, CSV_FILE
+from constants import REFRESH_SECONDS
 
-get_clients()
 
 # Load all styles FIRST - this ensures they persist on refresh/rerun
 load_styles()
