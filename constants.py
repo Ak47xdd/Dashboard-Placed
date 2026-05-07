1ff259a2117a2c226195a61d41ed74bf70699f32
Akshay_Babu <akshaysureshbabu200@gmail.com>
2026-05-07 11:18:05 +0530

    Remove Supabase credentials from constants.py
    Removed sensitive Supabase URL and key from constants.py.

--- constants.py
+++ constants.py
@@ -12,8 +12,7 @@ MAIN_CSV = "STUDENTS - Sheet1"
 CSV_FILE = "CLASSIFICATIONS - Sheet1 (1).csv"
 
 # ---- Supabase connection ----
-SUPABASE_URL = "https://bzvztzxrrziqrfokcyuf.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6dnp0enhycnppcXJmb2tjeXVmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Nzk5MzMyNCwiZXhwIjoyMDkzNTY5MzI0fQ.HAUOI6sm4EOLKcKPScbkeA7fkt6Vtx0Iq2hdzRiXfaY"
+# add
 
 # Table schema provided by you: public."CLASSIFICATION"
 SUPA_DB = "CLASSIFICATION"
