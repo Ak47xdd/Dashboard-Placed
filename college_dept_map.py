798a432fabc1bd35c253b2868106d40d5803abca
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-05-12 15:46:50 +0530

    college_dept

--- college_dept_map.py
+++ college_dept_map.py
@@ -6,6 +6,7 @@ COLLEGE_VARIANT_CANONICAL_MAP = {
     "amrita": "Amrita Vishwa Vidyapeetham Mysore",
     "amrita vishwa vidyapeetham mysuru": "Amrita Vishwa Vidyapeetham Mysore",
     "amrita vishwa vidyapeetham": "Amrita Vishwa Vidyapeetham Mysore",
+    "government college for women": "Government College For Women, Thiruvananthapuram",
 }
 
 # wrong/spelling-variant(lowercase) -> canonical department name(Display name)
@@ -14,4 +15,5 @@ DEPARTMENT_VARIANT_CANONICAL_MAP = {
     "integrated mca": "BCA + MCA",
     "computer applications": "BCA",
     "bcom finance": "BCom",
+    "ba english": "BA English",
 }
