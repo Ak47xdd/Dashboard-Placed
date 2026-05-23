c860da070c6506031e7bdd604d0a4503de79a2af
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-05-23 12:49:35 +0530

    wake

--- wake.py
+++ wake.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import TimeoutExceptionpip
+from selenium.common.exceptions import TimeoutException
 import os
 
 # Streamlit app URL from environment variable (or default)
