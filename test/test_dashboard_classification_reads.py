137c8e1b60664cdf154ba125cf1ae8b51183b54d
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-06-19 14:25:28 +0530

    Changes

--- test/test_dashboard_classification_reads.py
+++ test/test_dashboard_classification_reads.py
@@ -41,7 +41,6 @@ class _FakeTable:
             rows = self._client.read_data_by_table.get(self._table_name, [])
             return _FakeResponse(data=list(rows))
 
-        # Insert path not used
         self._client.insert_calls.append((self._table_name, dict(self._insert_payload)))
         return _FakeResponse(data=list(self._client.insert_result_rows))
 
